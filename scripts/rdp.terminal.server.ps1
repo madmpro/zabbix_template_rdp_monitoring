888b2d6397dd9a7cf379b1967df4da81aea91bb9
madmpro <32668768+madmpro@users.noreply.github.com>
2020-01-12 19:01:35 +0300

    Update rdp.terminal.server.ps1
    SWITCH statment wil work quicker than IF. 
    This will resolve error (ZBX_NOTSUPPORTED: Timeout while executing a shell script.)

--- scripts/rdp.terminal.server.ps1
+++ scripts/rdp.terminal.server.ps1
@@ -1,4 +1,4 @@
-# Improved by Ilias Aidar - 24/09/2019
+# Improved by madm.pro
 # Originally created by Diego Cavalcante - 10/02/2017
 # Monitoring Windows RDP - Terminal Server
 
@@ -6,44 +6,45 @@ Param(
   [string]$select
 )
 
-# Active Users: Domain Name, Username, Computer Name, IP Address
-if ( $select -eq 'ACTIVE' )
-{
-Import-Module PSTerminalServices
-Get-TSSession -State Active -ComputerName localhost | foreach {$_.DomainName, $_.UserName, $_.ClientName, (($_.IPAddress).IPAddressToString), ""}
-}
 
-# Total Active Users
-if ( $select -eq 'ACTIVENUM' )
-{
 Import-Module PSTerminalServices
-Get-TSSession -State Active -ComputerName localhost | foreach {$_.UserName} | Measure-Object -Line | select-object Lines | select-object -ExpandProperty Lines
-}
 
-# Inactive Users: Domain Name, Username
-if ( $select -eq 'INACTIVE' )
-{
-Import-Module PSTerminalServices
-Get-TSSession -State Disconnected -ComputerName localhost | where { $_.SessionID -ne 0 } | foreach {$_.DomainName, $_.UserName, ""}
-}
+Switch ($select){
 
-# Toal Inactive Users
-if ( $select -eq 'INACTIVENUM' )
-{
-Import-Module PSTerminalServices
-Get-TSSession -State Disconnected -ComputerName localhost | where { $_.SessionID -ne 0 } | foreach {$_.UserName} | Measure-Object -Line | select-object Lines | select-object -ExpandProperty Lines
-}
 
-# List of Remote Computer Names
-if ( $select -eq 'DEVICE' )
-{
-Import-Module PSTerminalServices
-Get-TSSession -State Active -ComputerName localhost | foreach {$_.ClientName}
-}
+    # Active Users: Domain Name, Username, Computer Name, IP Address
+    'ACTIVE'
+    {
+        Get-TSSession -State Active -ComputerName localhost | foreach {$_.DomainName, $_.UserName, $_.ClientName, (($_.IPAddress).IPAddressToString), ""}
+    }
 
-# List of Remoter IP Addresses
-if ( $select -eq 'IP' )
-{
-Import-Module PSTerminalServices
-Get-TSSession -State Active -ComputerName localhost | foreach {(($_.IPAddress).IPAddressToString)}
+    # Total Active Users
+    'ACTIVENUM'
+    {
+        Get-TSSession -State Active -ComputerName localhost | foreach {$_.UserName} | Measure-Object -Line | select-object Lines | select-object -ExpandProperty Lines
+    }
+
+    # Inactive Users: Domain Name, Username
+    'INACTIVE'
+    {
+        Get-TSSession -State Disconnected -ComputerName localhost | where { $_.SessionID -ne 0 } | foreach {$_.DomainName, $_.UserName, ""}
+    }
+
+    # Toal Inactive Users
+    'INACTIVENUM'
+    {
+        Get-TSSession -State Disconnected -ComputerName localhost | where { $_.SessionID -ne 0 } | foreach {$_.UserName} | Measure-Object -Line | select-object Lines | select-object -ExpandProperty Lines
+    }
+
+    # List of Remote Computer Names
+    'DEVICE'
+    {
+        Get-TSSession -State Active -ComputerName localhost | foreach {$_.ClientName}
+    }
+
+    # List of Remoter IP Addresses
+    'IP'
+    {
+        Get-TSSession -State Active -ComputerName localhost | foreach {(($_.IPAddress).IPAddressToString)}
+    }
 }
